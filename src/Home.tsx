609893fc2ea7d3d9a6193dab1f1bb81ec0d30b5d
whannallah <whannallah@gmail.com>
2022-12-18 17:45:18 -0500

    fixed spelling

--- src/Home.tsx
+++ src/Home.tsx
@@ -12,8 +12,8 @@ function Home(){
         <div style ={{padding: 100}}>
         <p id="heading" aria-label="welcome heading"> Welcome to Matched. </p>
         <p id="body-text" aria-label="description of the application's mission">  Our goal is to increase connection among students at Brown. 
-        To form a new connection, all you have to do is specify your match type (study buddy, date, or friend) then fill out a quick questionnaire.
-         We then use a natural language processing model, Cohere, to score the similarity between your answers to the questionnaire and others' ansers. Our 
+        To form a new connection, all you have to do is specify your match type (study buddy, date, or friend), then fill out a quick questionnaire.
+         We then use a natural language processing model, Cohere, to score the similarity between your answers to the questionnaire and others' answers. Our 
          algorithm takes into account the similarity between each question equally and returns your top 3 matches! </p>
          <p id="body-text">
          When using Matched, keep in mind that meeting new people based on similarity isn't the only way. We encourage
